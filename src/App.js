4b590ced3af5a2cd739518f0dc825e41531c29bb
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-26 17:32:29 +0200

    Teams

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Teams</h1>
+        <h1 className="text-center">Teams</h1>
         <div className="row">
           {teams.map(element => <Team team={element} />)}
         </div>
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -2,9 +2,16 @@ import React from "react"
 
 class Team extends React.Component {
     render() {
+        const {shortName, crestUrl, venue, address, email, phone, founded} = this.props.team
         return (
-            <div>
-                hello
+            <div className="col-4 my-4">
+                <img className="img-fluid" src={crestUrl}/>
+                <p>{shortName}</p>
+                <p>{venue}</p>
+                <p>{address}</p>
+                <p>{email}</p>
+                <p>{phone}</p>
+                <p>{founded}</p>
             </div>
         )
     }
