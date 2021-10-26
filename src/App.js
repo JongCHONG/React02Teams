a9167dab75d16f68f16312032eb2a1861e88d51e
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-26 17:16:45 +0200

    Team

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import teams from "./teams.json"
+import Team from "./components/Teams"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Teams</h1>
+        <div className="row">
+          {teams.map(element => <Team team={element} />)}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
