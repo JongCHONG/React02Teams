452336c628cc6ea357c025674e9d6f8917e0a77b
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-27 10:28:36 +0200

    Teamsfinal v2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,11 +8,11 @@ class Counter extends React.Component {
     render() {
         console.log(this.props);
         return (
-            <>
+            <div className="text-center">
                 <h2>{this.props.count}</h2>
                 <button onClick={this.props.substract}>-</button>
                 <button onClick={this.props.increment}>+</button>
-            </>
+            </div>
         )
     }
 }
